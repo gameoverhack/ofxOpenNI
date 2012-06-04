3ddba6146eed6cdf0d022788dac26342309b100c
Matthew Gingold <m@gingold.com>
2012-06-04 14:12:22 +0200

    Updated simple UserAndCloud example to show access to pixel and texture and mesh references

--- examples/opeNI-SimpleExamples/src-UserAndCloud-Simple/testApp.cpp
+++ examples/opeNI-SimpleExamples/src-UserAndCloud-Simple/testApp.cpp
@@ -3,76 +3,116 @@
 //--------------------------------------------------------------
 void testApp::setup() {
     
-    ofSetLogLevel(OF_LOG_NOTICE);
+    ofSetLogLevel(OF_LOG_VERBOSE);
 
     openNIDevice.setup();
-    //openNIDevice.setLogLevel(OF_LOG_VERBOSE);
-    openNIDevice.addDepthGenerator();
     openNIDevice.addImageGenerator();
-    openNIDevice.addUserGenerator();
-    
-    openNIDevice.setRegister(true); // register depth to image
+    openNIDevice.addDepthGenerator();
+    openNIDevice.setRegister(true);
     openNIDevice.setMirror(true);
+    openNIDevice.addUserGenerator();
+    openNIDevice.setMaxNumUsers(2);
+    openNIDevice.start();
     
-    openNIDevice.setMaxNumUsers(2); // defualt is 4
-    openNIDevice.setUseMaskTextureAllUsers(true);
+    // set properties for all user masks and point clouds
+    //openNIDevice.setUseMaskPixelsAllUsers(true); // if you just want pixels, use this set to true
+    openNIDevice.setUseMaskTextureAllUsers(true); // this turns on mask pixels internally AND creates mask textures efficiently
     openNIDevice.setUsePointCloudsAllUsers(true);
-    openNIDevice.setPointCloudDrawSizeAllUsers(3); // sets the size the points are drawn on screen (in pixels)
-    openNIDevice.setPointCloudResolutionAllUsers(2); // sets the step between pixels and point cloud eg., 1 is every pixel, 2 is every second pixel etc
-
-	openNIDevice.start();
-
+    openNIDevice.setPointCloudDrawSizeAllUsers(2); // size of each 'point' in the point cloud
+    openNIDevice.setPointCloudResolutionAllUsers(2); // resolution of the mesh created for the point cloud eg., this will use every second depth pixel
+    
+    // you can alternatively create a 'base' user class
+//    ofxOpenNIUser user;
+//    user.setUseMaskTexture(true);
+//    user.setUsePointCloud(true);
+//    user.setPointCloudDrawSize(2);
+//    user.setPointCloudResolution(2);
+//    openNIDevice.setBaseUserClass(user);
+      
     verdana.loadFont(ofToDataPath("verdana.ttf"), 24);
 }
 
 //--------------------------------------------------------------
 void testApp::update(){
-    ofBackground(0, 0, 0);
     openNIDevice.update();
 }
 
 //--------------------------------------------------------------
 void testApp::draw(){
 	ofSetColor(255, 255, 255);
-    openNIDevice.drawDebug(); // debug draw does the equicalent of the commented methods below
-    //openNIDevice.drawDepth(0, 0, 640, 480);
-    //openNIDevice.drawImage(640, 0, 640, 480);
-    //openNIDevice.drawSkeletons(640, 0, 640, 480);
     
-    // do some drawing of user clouds and masks
     ofPushMatrix();
+    // draw debug (ie., image, depth, skeleton)
+    openNIDevice.drawDebug();
+    ofPopMatrix();
+    
+    ofPushMatrix();
+    // use a blend mode so we can see 'through' the mask(s)
     ofEnableBlendMode(OF_BLENDMODE_ALPHA);
+    
+    // get number of current users
     int numUsers = openNIDevice.getNumTrackedUsers();
-    for (int nID = 0; nID < numUsers; nID++){
-        ofxOpenNIUser & user = openNIDevice.getTrackedUser(nID);
+    
+    // iterate through users
+    for (int i = 0; i < numUsers; i++){
+        
+        // get a reference to this user
+        ofxOpenNIUser & user = openNIDevice.getTrackedUser(i);
+        
+        // draw the mask texture for this user
         user.drawMask();
+        
+        // you can also access the pixel and texture references individually:
+        
+        // TEXTURE REFERENCE
+        //ofTexture & tex = user.getMaskTextureReference();
+        // do something with texture...
+        
+        // PIXEL REFERENCE
+        //ofPixels & pix = user.getMaskPixels();
+        // do something with the pixels...
+        
+        // and point clouds:
+        
         ofPushMatrix();
-        ofTranslate(320, 240, -1000);
+        // move it a bit more central
+        ofTranslate(320, 240, 10);
         user.drawPointCloud();
+        
+        // you can also access the mesh:
+        
+        // MESH REFERENCE
+        //ofMesh & mesh = user.getPointCloud();
+        // do something with the point cloud mesh
+        
         ofPopMatrix();
+        
     }
+    
     ofDisableBlendMode();
     ofPopMatrix();
     
+    // draw some info regarding frame counts etc
 	ofSetColor(0, 255, 0);
-	string msg = " MILLIS: " + ofToString(ofGetElapsedTimeMillis()) + " FPS: " + ofToString(ofGetFrameRate());
-	verdana.drawString(msg, 20, 506);
+	string msg = " MILLIS: " + ofToString(ofGetElapsedTimeMillis()) + " FPS: " + ofToString(ofGetFrameRate()) + " Device FPS: " + ofToString(openNIDevice.getFrameRate());
+    
+	verdana.drawString(msg, 20, openNIDevice.getNumDevices() * 480 - 20);
+}
+
+//--------------------------------------------------------------
+void testApp::userEvent(ofxOpenNIUserEvent & event){
+    // show user event messages in the console
+    ofLogNotice() << getUserStatusAsString(event.userStatus) << "for user" << event.id << "from device" << event.deviceID;
 }
 
 //--------------------------------------------------------------
 void testApp::exit(){
-    // this often does not work -> it's a known bug -> but calling it on a key press or 
-    // anywhere that isnt std::aexit() works...press 'x' to shutdown cleanly...
     openNIDevice.stop();
 }
 
 //--------------------------------------------------------------
 void testApp::keyPressed(int key){
-    switch (key) {
-        case 'x':
-            openNIDevice.stop();
-            break;
-    }
+
 }
 
 //--------------------------------------------------------------
@@ -104,4 +144,3 @@ void testApp::mouseReleased(int x, int y, int button){
 void testApp::windowResized(int w, int h){
 
 }
-
